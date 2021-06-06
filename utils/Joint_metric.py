75c2334f4bb19e58efa0e3aae23d27190dc35382
Sambaran <sambaranghosal@gmail.com>
2021-06-06 09:49:28 -0700

    Update Joint_metric.py

--- utils/Joint_metric.py
+++ utils/Joint_metric.py
@@ -2,16 +2,28 @@ import torch
 # pos_preds is (N_actors, N_modes, T, 2)
 # probs is (N_modes)
 # GT is (N_actors, T, 2)
-def min_K_joint_ADE_metric(pos_preds, GT):
+def min_K_joint_ADE_metric(pos_preds, GT, GT_mask):
     pred_size = list(pos_preds.size())
     T = pred_size[2]
     
     GT = GT[:,None,:,:]
+    GT_mask = GT_mask[:,None,:]
+    
+#     print(pos_preds[0,0,:,0])
+#     print(pos_preds[0,0,:,1])
+#     print(pos_preds[0,1,:,0])
+#     print(pos_preds[0,1,:,1])
+#     print(pos_preds[0,2,:,0])
+#     print(pos_preds[0,2,:,1])
+#     print(GT[0,0,:,0])
+#     print(GT[0,0,:,0])
+    
     
     # shape (N_actors, N_modes, T, 2)
     sq_dif = torch.square(pos_preds - GT)
     # shape (N_actors, N_modes, T)
     L2_per_timestep = torch.sqrt(torch.sum(sq_dif, 3))
+    L2_per_timestep = L2_per_timestep * GT_mask.float()
     # shape (N_actors, N_modes)
     ADE_per_actor_per_mode = torch.sum(L2_per_timestep, 2) / T
     # shape (N_modes)
